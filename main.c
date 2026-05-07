956090dd9deceaf66ea5901b1d30acf23170f3cc
Krzysztof Murawski <murek.krzys@gmail.com>
2026-05-07 16:58:06 +0200

    Working cp

--- main.c
+++ main.c
@@ -1,65 +1,72 @@
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
 
 
+
 int main(int argc, char *argv[]){
-    
     if (argc < 3){
-        printf("help message: mycp [source_file] [dest_dir]\n");
-        return 0;
+        printf("usage: mycp [source_file] [dest_dir]\n");
+        return 1;
     }
 
-    char source_file[32];
-    char dest_dir[32];
-    strcat(source_file, argv[1]);
-    strcat(dest_dir, argv[2]);
 
-    printf("src dest / %s %s\n", source_file, dest_dir);
-    
+    char *source_file = argv[1];
+    char *dest_dir = argv[2];
+
+    char dest_path[256] = {0};
+    strcat(dest_path, dest_dir);
+    strcat(dest_path, source_file);
+
     int source_fd = open(source_file, O_RDONLY);
     if (source_fd < 0){
         perror("Open Source File");
         return 1;
     }
 
-
-    char dest_path[256];
-    char filename[32];
-    // int i = 11;//sizeof(argv[1]);
-    // printf("%s\n", argv[1]);
-    // while(i > 0 && source_file[i] != '/'){
-    //     printf("> %s\n", source_file[i]);
-    //     --i;
-    // }
-    // int j = i;
-    // while (j < sizeof(source_file)){
-    //     filename[j-i] = source_file[j];
-    //     ++j;
-    // }
-    // printf("%d %s\n", i, filename);
-
-
-    strcat(dest_path, dest_dir);
-    strcat(dest_path, source_file);
-    printf("%s\n", dest_path);
-    int dest_fd = creat(dest_path, O_CREAT); // no such file or dir...
+    int dest_fd = creat(dest_path, 0777);
     if (dest_fd < 0){
         perror("Open Destination File");
         return 1;
     }
+    
+    ssize_t bytes_read = 0;
+
+    while(1){
+
+        char buffer[64] = {0};
+        bytes_read = read(source_fd, buffer, sizeof(buffer));
+        if (bytes_read == 0){
+            break;
+        } else if (bytes_read < 0){
+            perror("Reading");
+            return 1;
+        }
+
+        int total_written = 0;
+
+        while (bytes_read > total_written){
+
+            ssize_t bytes_written = write(dest_fd, buffer, sizeof(buffer));
+            if (bytes_written < 0){
+                perror("Writing to file");
+                return 1;
+            }
+
+            total_written = total_written + bytes_written;
+        }
+
+
+    }
 
 
 
-    if (close(dest_fd) != 0){
-        perror("Closing Destination File");
-        return 1;
-    };
-    if (close(source_fd) != 0){
-        perror("Closing Source File");
-        return 1;
-    };
+    close(dest_fd);
+    close(source_fd);
     return 0;
 }
+
+
